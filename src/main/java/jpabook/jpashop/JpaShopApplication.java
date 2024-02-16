bf441f2a29effa1ba88873caa4b8cf74ef35f740
Kim So-Myoung <thaud9696@naver.com>
2024-02-16 17:26:48 +0900

    20240216 : 연습 및 테스트용 코드 삭제, member, order 도메인 설계
    20240216 : 연습 및 테스트용 코드 삭제, member, order 도메인 설계

--- src/main/java/jpabook/jpashop/JpaShopApplication.java
+++ src/main/java/jpabook/jpashop/JpaShopApplication.java
@@ -7,12 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpaShopApplication {
 	
 	public static void main(String[] args) {
-//		lombok 테스트
-/*		Hello hello = new Hello();
-		hello.setData("hello lombok");
-		String data = hello.getData();
-		System.out.println("data = " + data);*/
-
 		SpringApplication.run(JpaShopApplication.class, args);
 	}
 
